9543dcad05c45ced0c5a48cc4c6e0d5ee7a5f36e
Jackie1019 <davidkale892@gmail.com>
2021-12-18 07:07:24 +0300

    re

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const app = express();
-
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, './', 'build');
 
 
 app.use(express.static(publicPath));
